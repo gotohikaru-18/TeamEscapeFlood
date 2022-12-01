ed13cd5f5ace706e825cff5bbe341eb0002699e6
輝 後藤 <121pg1001@tokyo.amg.ac.jp>
2022-12-02 00:58:58 +0900

    2Dの当たり判定実装中

---
+++ Source/EscapeFlood/GimmickBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ObjectBase.h"
+#include "GImmickBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEFLOOD_API AGImmickBase : public AObjectBase
+{
+	GENERATED_BODY()
+	
+};
